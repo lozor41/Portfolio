28c0f9600aed801e1734682a714a6052c08834bb
Lozor41 <pierre-8992@outlook.fr>
2024-03-29 20:43:12 +0100

    Up gh pages

--- src/components/Explore/ExploreLayout.js
+++ src/components/Explore/ExploreLayout.js
@@ -8,7 +8,7 @@ export default function ExploreLayout() {
                 <p className="ex-p"> Voici ici quelques projets que j'ai déjà réalisé au cours de ma formation.</p>
                 <nav>
                     <ul className="explore-nav m-0 h-100 d-flex justify-content-between align-items-center list-unstyled">
-                        <li><NavLink to="/"><span></span> Projets</NavLink></li>
+                        <li><NavLink to="/Portfolio"><span></span> Projets</NavLink></li>
                         <li><NavLink to="about"><span></span> A propos</NavLink></li>
                     </ul>
                 </nav>
